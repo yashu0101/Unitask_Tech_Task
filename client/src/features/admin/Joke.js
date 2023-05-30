26efd6ae9d58f57ae303c237d4665be7e1abc367
yashu0101 <chavanyash0604@gmail.com>
2023-05-30 18:32:48 +0530

    add

--- client/src/features/admin/Joke.js
+++ client/src/features/admin/Joke.js
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from "react";
+
 const Joke = () => {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => setJoke(data.value))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
-      <h2>Joke</h2>
+      <h2 style={{ fontSize: "20px" }}>JOKE:- {joke}</h2>
     </>
   );
 };
